677256b36a3bb9ddb6c400166b859d705a61bd6f
Anurag Pandey <arushpandey123buy@gmail.com>
2022-06-26 14:55:21 +0530

    ok

--- src/Components/About/AboutTop.jsx
+++ src/Components/About/AboutTop.jsx
@@ -1,20 +1,18 @@
-const AboutTop =()=>{
-    return(
+const AboutTop = () => {
+    return (
         <>
-        <div className="about_Top_outrt_box">
-            <div className="about_left_top">
-                <h1>Who We Are</h1>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem aperiam magnam delectus nulla sint voluptas dolores, architecto eaque. Tempora quo culpa ad sint adipisci vitae, repellat inventore, a quibusdam ea aliquid voluptate nulla consectetur corrupti velit facere rem error fugiat mollitia sit ipsa. Sed corporis ut repudiandae ab esse eveniet.
-                </p>
-            </div>
-            <div className="about_right_top">
+            <div className="about_Top_outrt_box">
+                <div className="about_left_top">
+                    <h1>Who We Are</h1>
+                    <br /><br />
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero quia nesciunt consectetur fugit sit vero, necessitatibus quidem illo iste dolor dignissimos iure minus exercitationem porro repellendus laudantium, corrupti rerum. Consequatur repellendus a nemo necessitatibus, fugiat deleniti odit perferendis sint placeat ducimus, enim culpa fugit suscipit laboriosam numquam? Voluptatem magnam dicta earum qui suscipit reiciendis quae labore hic corrupti officiis laudantium non molestias, ea praesentium ullam delectus, culpa adipisci. Quam, magni?
+                    </p>
+                </div>
+                <div className="about_right_top">
                     <iframe width="560" height="315" src="https://www.youtube.com/embed/ardtvdR28SQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
             </div>
-        </div>
-        
-        
-        
         </>
     )
 }
