3fa7ca720297e36dc47ca2d0a116a0837f97a206
Anurag Pandey <arushpandey123buy@gmail.com>
2022-07-29 12:55:48 +0530

    ok

--- src/Components/Home/homeSecTop.jsx
+++ src/Components/Home/homeSecTop.jsx
@@ -30,8 +30,7 @@ const HomeSecTop = () => {
                         <div className="cont_header_second">
                             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore quaerat velit necessitatibus at enim vel reprehenderit est corrupti quae ab id eius, doloribus rem dolores! Voluptate quam voluptas laboriosam quis.</p><br />
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In nemo obcaecati saepe tenetur quae ipsum deserunt est, non voluptatum, magnam velit dicta eos alias doloribus consequuntur quidem perferendis. Laborum, quos.</p><br />
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut pariatur optio iure fuga eveniet nisi odio quam! Iusto, laudantium vero. Sapiente ut, commodi nulla soluta repellendus fugiat vitae asperiores doloremque!
-                            </p>
+                            
                             <br />
                         </div>
 
